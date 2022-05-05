508ad5db4a1c9565fd2d784c6048f733b639ed2b
Renan Lima <renangenito@hotmail.com>
2022-05-05 12:57:39 -0300

    Ínicio do projeto!

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Formulario from "./Components/Formulario";
+import Lista from "./Components/Lista";
+function App() {
+  return (
+    <div className="App">
+    <Formulario />
+    <Lista />
+    </div>
+  );
+}
+
+export default App;
